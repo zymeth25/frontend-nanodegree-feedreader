15e8059cdbbab49ee3536b28052a032935ff5ef4
Klemens Starybrat <klemens.starybrat@gmail.com>
2018-04-28 17:43:52 +0200

    test: Add a spec for loading a new feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -106,9 +106,25 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        let oldContent, newContent;
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                oldContent = $('.feed').html();
+                loadFeed(1, done);
+            });
+        });
+
+        it('changes content', function(done) {
+            newContent = $('.feed').html();
+
+            expect(newContent).not.toBe(oldContent);
+            done();
+        });
+    });
 }());
